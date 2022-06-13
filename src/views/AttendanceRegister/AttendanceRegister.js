a331a07d18b90736b6bb9405b398b6622c1501ff
diogocerutti <diogo.cerutti19@gmail.com>
2022-06-13 20:31:02 -0300

    Comentário

--- src/views/AttendanceRegister/AttendanceRegister.js
+++ src/views/AttendanceRegister/AttendanceRegister.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import { Button, TextareaAutosize } from '@mui/material';
+import { Button } from '@mui/material';
 
 export default function AttendanceRegister() {
 
@@ -23,11 +23,11 @@ export default function AttendanceRegister() {
               label="Descrição"
               fullWidth
               autoComplete="given-name"
-              variant="standard"
+              variant="standards"
             />
           </Grid>          
             <Grid item xs={12} sm={6} lg={5.5}>
-                <TextField
+              <TextField
                 required
                 id="lastName"
                 name="lastName"
@@ -37,10 +37,8 @@ export default function AttendanceRegister() {
                 variant="outlined"
                 multiline
                 rows={10}
-                />
-                {/* <TextareaAutosize minRows={5}/> */}
+              />
             </Grid>
-            
           </Grid>
         <Grid item xs={12} sx={{ marginTop: '20px' }}>
           <TextField
@@ -66,10 +64,10 @@ export default function AttendanceRegister() {
             />
         </Grid>
         <Grid item xs={12} sm={6} lg={6.5} sx={{ marginTop: '20px' }}>
-            <TextField            
-              variant="outlined"
-              type="file"
-            />
+          <TextField            
+            variant="outlined"
+            type="file"
+          />
         </Grid>
         <Grid container textAlign="center">
           <Grid item xs={4} sm={4} lg={4} sx={{ marginTop: '20px' }}>
